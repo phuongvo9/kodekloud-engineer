49eb238c425c9ad9479538246d4586fa7f89241f
phuongvo9 <huyphuong4116@gmail.com>
2021-12-03 19:52:22 +0700

    add cron file

---
+++ SystemAdministrator/create-cron-job.sh
@@ -0,0 +1,22 @@
+# Nautilus App 1
+ssh tony@stapp1
+Ir0nM@n
+# Nautilus App 2
+ssh steve@stapp02
+Am3ric@
+# Nautilus App 3
+ssh banner@stapp03
+BigGr33n
+
+# -------------------------------------------------------------------------------
+yum install cronie
+systemctl start crond.service
+systemctl status crond.service
+
+crontab -u root -l
+crontab -e 
+    # input cron */5 * * echo hello > /tmp/cron_text
+
+watch -n 5 ls /tmp
+
+
