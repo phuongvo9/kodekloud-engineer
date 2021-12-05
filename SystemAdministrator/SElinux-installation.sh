48f1e4fc3f0c9a49c887fd17b29fd18dd95cbfb5
phuongvo9 <huyphuong4116@gmail.com>
2021-12-05 21:01:54 +0700

    completed task SE linux

--- SystemAdministrator/SElinux-installation.sh
+++ SystemAdministrator/SElinux-installation.sh
@@ -1,7 +1,10 @@
 
-
+# Ref ID:61ab7511c15fbfc443e0d85d
 
 yum install selinux*
 
 cat /etc/selinux/config | grep selinux
 cat /etc/selinux/config | grep SELINUX
+vi /etc/selinux/config
+
+# Change SELINUX from enforcing to disabled
