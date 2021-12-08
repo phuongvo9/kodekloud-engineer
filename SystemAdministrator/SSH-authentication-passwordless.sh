9e560d467910423f44d0928250e6b3c7669834d7
phuongvo9 <huyphuong4116@gmail.com>
2021-12-08 22:16:42 +0700

    add SSH authentication

---
+++ SystemAdministrator/SSH-authentication-passwordless.sh
@@ -0,0 +1,17 @@
+# Nautilus App 1
+ssh tony@stapp1
+Ir0nM@n
+# Nautilus App 2
+ssh steve@stapp02
+Am3ric@
+# Nautilus App 3
+ssh banner@stapp03
+BigGr33n
+
+# Nautilus DB Server
+ssh peter@stdb01
+Sp!dy
+
+#HTTP LBR
+ssh loki@stlb01
+Mischi3f
