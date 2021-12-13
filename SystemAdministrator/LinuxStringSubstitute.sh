fe762557144f9f486df06e40cedaadea4d1fa107
phuongvo9 <huyphuong4116@gmail.com>
2021-12-13 21:27:20 +0700

    completed sed -i /s /g

---
+++ SystemAdministrator/LinuxStringSubstitute.sh
@@ -0,0 +1,15 @@
+# Nautilus Backup Server
+stbkp01
+172.16.238.16 - stbkp01.stratos.xfusioncorp.com
+ssh clint@stbkp01
+H@wk3y3
+
+ssh clint@stbkp01 
+H@wk3y3
+
+sudo sed -i 's/Text/Submarine/g' nautilus.xml
+
+cat nautilus.xml | grep "Text"
+cat nautilus.xml | grep "Submarine"
+
+
