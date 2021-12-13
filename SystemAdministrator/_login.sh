0be780c1fb02b532f913ca5145db93f19d3db76d
phuongvo9 <huyphuong4116@gmail.com>
2021-12-13 21:19:23 +0700

    update Login in Nautilus

--- SystemAdministrator/_login.sh
+++ SystemAdministrator/_login.sh
@@ -18,3 +18,24 @@ Sp!dy
 #HTTP LBR
 ssh loki@stlb01
 Mischi3f
+
+# Nautilus Storage Server
+ststor01 - 172.16.238.15 - ststor01.stratos.xfusioncorp.com
+natasha@ststor01
+natasha
+Bl@kW
+
+# Nautilus Backup Server
+stbkp01
+172.16.238.16 - stbkp01.stratos.xfusioncorp.com
+ssh clint@stbkp01
+H@wk3y3
+
+# Nautilus Mail Server
+stmail01
+172.16.238.17 - stmail01.stratos.xfusioncorp.com
+ssh groot@stmail01
+groot
+Gr00T123
+
+
