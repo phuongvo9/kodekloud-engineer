d40ac4b597d1f77bb6e180cd5c3b07c7109652b5
phuongvohuy9 <huyphuong4116@gmail.com>
2021-12-22 11:33:18 +0700

    complete Linux Services

---
+++ SystemAdministrator/LinuxServices.sh
@@ -0,0 +1,15 @@
+
+
+# Nautilus App 1
+ssh tony@stapp01
+Ir0nM@n
+# Nautilus App 2
+ssh steve@stapp02
+Am3ric@
+# Nautilus App 3
+ssh banner@stapp03
+BigGr33n
+    1  sudo yum install nscd
+    2  sudo systemctl enable nscd
+    3  sudo systemctl start nscd
+    4  history
