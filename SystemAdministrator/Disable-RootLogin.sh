8c4dd2de093c3ea53aaa74bc89124185c55c9ece
phuongvohuy9 <huyphuong4116@gmail.com>
2021-12-30 22:17:09 +0700

    update disable root login

--- SystemAdministrator/Disable-RootLogin.sh
+++ SystemAdministrator/Disable-RootLogin.sh
@@ -1,5 +1,12 @@
-[root@stapp02 steve]# history
-    1  vi /etc/ssh/sshd_config
-    2  cat /etc/ssh/sshd_config | grep "Permit"
-    3  systemctl restart sshd
-    4  history
+
+
+systemctl status sshd
+vi /etc/ssh/sshd_config
+# enable: PermitRootLogin no
+systemctl restart sshd
+
+# [root@stapp02 steve]# history
+#     1  vi /etc/ssh/sshd_config
+#     2  cat /etc/ssh/sshd_config | grep "Permit"
+#     3  systemctl restart sshd
+#     4  history
