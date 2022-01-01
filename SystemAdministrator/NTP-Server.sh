f5811abda62819b1926251ee9fd66eb01eaffdca
phuongvohuy9 <huyphuong4116@gmail.com>
2022-01-01 20:25:44 +0700

    add NTP server file

---
+++ SystemAdministrator/NTP-Server.sh
@@ -0,0 +1,7 @@
+# Install and configure NTP server on App Server 2.
+
+# Add NTP server 3.pool.ntp.org in NTP configuration on App Server 2.
+
+# Please do not try to start/restart/stop ntp service
+# as we already have a restart for this service scheduled for tonight and we don't want these changes to be applied right now.
+
