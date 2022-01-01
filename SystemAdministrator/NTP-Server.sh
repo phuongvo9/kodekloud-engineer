c42b860b8599eb5b024a5e6035a3dc78a4b45be8
phuongvohuy9 <huyphuong4116@gmail.com>
2022-01-01 20:55:40 +0700

    update NTP server installation

--- SystemAdministrator/NTP-Server.sh
+++ SystemAdministrator/NTP-Server.sh
@@ -5,3 +5,10 @@
 # Please do not try to start/restart/stop ntp service
 # as we already have a restart for this service scheduled for tonight and we don't want these changes to be applied right now.
 
+[steve@stapp02 ~]$ history
+    1  sudo rpm -qa | grep NTP
+    2  sudo yum install ntp
+    3  sudo vi /etc/ntp.conf 
+    4  cat /etc/ntp | grep europe
+    5  cat /etc/ntp.conf | grep europe
+    6  history
