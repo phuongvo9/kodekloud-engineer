906b6255be0ed767fd6301c226463cc9b98d3387
phuongvohuy9 <huyphuong4116@gmail.com>
2022-01-07 14:46:08 +0700

    update completion

--- SystemAdministrator/Linux-Bash-Script-KK.sh
+++ SystemAdministrator/Linux-Bash-Script-KK.sh
@@ -19,12 +19,6 @@ scp -i /home/steve/.ssh/id_rsa /backup/xfusioncorp_beta.zip clint@stbkp01:/backu
 
 #####
 
-# Nautilus App 1
-ssh tony@stapp01
-Ir0nM@n
-# Nautilus App 2
-ssh steve@stapp02
-Am3ric@
 # Nautilus App 3
 ssh banner@stapp03
 BigGr33n
@@ -44,3 +38,16 @@ ssh-copy-id clint@stbkp01
 
 ssh clint@stbkp01
 H@wk3y3
+
+###### ---
+# The production support team of xFusionCorp Industries is working on developing some bash scripts to automate different day to day tasks. One is to create a bash script for taking websites backup. They have a static website running on App Server 3 in Stratos Datacenter, and they need to create a bash script named blog_backup.sh which should accomplish the following tasks. (Also remember to place the script under /scripts directory on App Server 3)
+
+
+
+# a. Create a zip archive named xfusioncorp_blog.zip of /var/www/html/blog directory.
+
+# b. Save the archive in /backup/ on App Server 3. This is a temporary storage, as backups from this location will be clean on weekly basis. Therefore, we also need to save this backup archive on Nautilus Backup Server.
+
+# c. Copy the created archive to Nautilus Backup Server server in /backup/ location.
+
+# d. Please make sure script won't ask for password while copying the archive file. Additionally, the respective server user (for example, tony in case of App Server 1) must be able to run it.
