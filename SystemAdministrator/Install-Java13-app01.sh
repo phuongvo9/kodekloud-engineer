283ff9300b5ca78c7c69a7ed417aad78cb1d7a40
phuongvohuy9 <huyphuong4116@gmail.com>
2022-01-14 21:20:19 +0700

    set sys env PATH for java

--- SystemAdministrator/Install-Java13-app01.sh
+++ SystemAdministrator/Install-Java13-app01.sh
@@ -9,3 +9,13 @@ sudo curl https://download.java.net/java/GA/jdk13.0.2/d4173c853231432d94f001e99d
 sudo tar -xf /opt/openjdk-13.0.2_linux-x64_bin.tar.gz -C /opt/
 # verify run 
 /opt/jdk-13.0.2/bin/java -version
+
+
+# We need to set java binary path in environment PATH variable to use java binaries. \
+# So that you can simply run java instead of the full path.
+
+export PATH=$PATH:/opt/jdk-13.0.2/bin
+
+#[thor@app01 ~]$ $PATH
+#-bash: /usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/home/thor/.local/bin:/home/thor/bin:/opt/jdk-13.0.2/bin: No such file or directory
+java -version
