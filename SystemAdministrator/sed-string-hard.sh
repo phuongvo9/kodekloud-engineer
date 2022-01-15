94fabb089fba2f8ec9b5e2e234496eab9e3b70b0
phuongvohuy9 <huyphuong4116@gmail.com>
2022-01-15 19:47:01 +0700

    loop

--- SystemAdministrator/sed-string-hard.sh
+++ SystemAdministrator/sed-string-hard.sh
@@ -8,3 +8,10 @@ cat /home/BSD_REPLACE.txt | grep "from"
 # [root@stapp02 home]# sed 's/\bfrom\b/their/g' /home/BSD.txt > /home/BSD_REPLACE.txt
 # [root@stapp02 home]# cat /home/BSD_DELETE.txt | grep "code"
 # [root@stapp02 home]# cat /home/BSD_REPLACE.txt | grep "from"
+
+
+
+for i in {1..35}; do
+   kubectl exec --namespace=kube-public curl -- sh -c 'test=`wget -qO- -T 2  http://webapp-service.default.svc.cluster.local:8080/info 2>&1` && echo "$test OK" || echo "Failed"';
+   echo ""
+done
