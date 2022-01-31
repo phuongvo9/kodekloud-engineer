bac85e659aa15ea4dd7f2237d665acab42a32b6e
phuongvo9 <huyphuong4116@gmail.com>
2022-01-31 17:49:57 +0700

    Deny incomming traffic to port 3002

--- SystemAdministrator/ApplicationSecurity_iptable.sh
+++ SystemAdministrator/ApplicationSecurity_iptable.sh
@@ -5,3 +5,6 @@ cat /etc/sysconfig/iptables
 # Allow port 8097
 iptables -A INPUT -p tcp --dport 8097 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
 
+# Deny incomming traffic to port 3002
+
+iptables -A INPUT -p tcp --dport 3002 -m conntrack --ctstate NEW,ESTABLISHED -j REJECT
